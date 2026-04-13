6ad3f540dc252fec48348dbc1394f9e47167de5b
AAsis-Shukl <169ashishkumar@gmail.com>
2026-04-13 21:56:33 +0530

    fixed static files problem

--- BCA_Pro/settings.py
+++ BCA_Pro/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,6 +140,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Base url to serve media files
 MEDIA_URL = '/media/'
 
